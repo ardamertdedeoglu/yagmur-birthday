aa1d3caa0a424a05defa85c9b6e7f4611471b666
Arda Mert Dedeoğlu <mert.dedeoglu@std.yildiz.edu.tr>
2026-02-05 20:12:41 +0300

    new story content v2

--- src/data/stories.js
+++ src/data/stories.js
@@ -13,26 +13,30 @@ export const stories = [
   },
   {
     id: 2,
-    text: "Her şey Nisan 2025'te başladı. Ya da en azından ben öyle hatırlıyorum. Pek iyi anmadığımız biri tarafından ekibimize katılmıştın. Bu ekibe katılmanın bizim için ne anlama geleceğini o zamanlar anlamamıştık tabi.",
+    text: "Bunları konuşuyor olmak istemezdim fakat 2 gündür bir baş ağrın var. Bunun nedeniyle de pek konuşamadık. Tekrardan en kısa sürede geçmesini diliyorum. Umarım bu mesajları okurken biraz daha iyisindir.",
   },
   {
     id: 3,
-    text: 'Hiçbir zaman pek konuşkan biri olmadım. Sen de tam aksi çok sosyal bir insandın. Ne kadar iyi biri olduğunu ilk günden beri anlamıştım. İçinde bulunduğumuz yarışma ortamında henüz yakın bir arkadaşlığımız olmamıştı. Ta ki hackathon gününe kadar.',
+    text: "Her şey Nisan 2025'te başladı. Ya da en azından ben öyle hatırlıyorum. Pek iyi anmadığımız biri tarafından ekibimize katılmıştın. Bu ekibe katılmanın bizim için ne anlama geleceğini o zamanlar anlamamıştık tabi.",
   },
   {
     id: 4,
-    text: 'O gün içinde bulunduğumuz takımı hiçbir zaman unutmam herhalde. Gerginliğim yanı sırasında böyle bir ekiple ve özellikle seninle beraber çalışmak çok keyifliydi. En çok o gün yakınlaştığımızı hissetmiştim.',
+    text: 'Hiçbir zaman pek konuşkan biri olmadım. Sen de tam aksi çok sosyal bir insandın. Ne kadar iyi biri olduğunu ilk günden beri anlamıştım. İçinde bulunduğumuz yarışma ortamında henüz yakın bir arkadaşlığımız olmamıştı. Ta ki hackathon gününe kadar.',
   },
   {
     id: 5,
-    text: 'Yarışmayı kazanamadık belki ama iyi vakit geçirdik. Yarışmadan sonra pek görüşemedik. Okulda ara sıra karşılaşmalarımız ve sosyal medyadan etkileşimlerimiz oldu sadece. Ta ki 20 Ocak 2026 gününe kadar.',
+    text: 'O gün içinde bulunduğumuz takımı hiçbir zaman unutmam herhalde. Gerginliğim yanı sırasında böyle bir ekiple ve özellikle seninle beraber çalışmak çok keyifliydi. En çok o gün yakınlaştığımızı hissetmiştim.',
   },
   {
     id: 6,
-    text: 'O günden beridir birbirimizi hiç tanımadığımız kadar yakından tanıdık, yakınlaştık. Senin için zor olan bir dönemde senin yanında olmaya çalıştım. Benim için oldukça değerli biri haline geldin. Yanında rahatça konuşabildiğim ve kendim olabildiğim biri oldun. Benim de senin için böyle olduğumu düşünüyorum.',
+    text: 'Yarışmayı kazanamadık belki ama iyi vakit geçirdik. Yarışmadan sonra pek görüşemedik. Okulda ara sıra karşılaşmalarımız ve sosyal medyadan etkileşimlerimiz oldu sadece. Ta ki 20 Ocak 2026 gününe kadar.',
   },
   {
     id: 7,
+    text: 'O günden beridir birbirimizi hiç tanımadığımız kadar yakından tanıdık, yakınlaştık. Senin için zor olan bir dönemde senin yanında olmaya çalıştım. Benim için oldukça değerli biri haline geldin. Yanında rahatça konuşabildiğim ve kendim olabildiğim biri oldun. Benim de senin için böyle olduğumu düşünüyorum.',
+  },
+  {
+    id: 8,
     text: 'Hayatımda biri için özel olarak web sitesiyle uğraşmamışımdır herhalde. Ama senin için değdiğini düşünüyorum. Umarım bu "küçük" :) sürprizi beğenmişsindir. Daha nice güzel anılar biriktireceğimiz harika bir yaş diliyorum sana. En kısa sürede yüz yüze görüşme dileğiyle. İyi ki doğdun Yağmur! 🎉',
   },
 ]
